e30e55368eee517740499bf3d7307872c4cd79e0
RajarshiBasu <rajobasu@rbasu.local>
2021-01-09 03:52:59 +0800

    added API for getting my requests

--- src/components/API/API.js
+++ src/components/API/API.js
@@ -19,3 +19,23 @@ export function dashboard(callback) {
     })
     .catch((Error) => console.log(Error));
 }
+
+export function requestsMadeByUser(callback) {
+  const actualURL = backendURL + "/social/requestsMade";
+
+  const params = new URLSearchParams();
+  params.append("user_id", sessionStorage.getItem("user_id"));
+  params.append("access_token", sessionStorage.getItem("access_token"));
+
+  axios({
+    method: "post",
+    url: actualURL,
+    data: params,
+    withCredentials: true,
+  })
+    .then((response) => {
+      console.log(response.data);
+      callback(response.data);
+    })
+    .catch((Error) => console.log(Error));
+}
